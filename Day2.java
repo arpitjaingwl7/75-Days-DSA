a58db0c4c1d0c249897a08cdef2266a7731ad637
Arpit Jain <arpitjaingwl7@gmail.com>
2025-01-02 15:18:18 +0530

    day 2

--- Day2.java
+++ Day2.java
@@ -1,7 +1,59 @@
 public class Day2 {
     public static void main(String[] args) {
-        System.out.println("Hello Worl2222d");
+      
     }
 }
 
-// byugyg
+
+// Java Sollution
+class Solution {
+    // Method to check if you can win Nim game
+    public boolean canWinNim(int n) {
+        // If the number of stones is a multiple of 4, you cannot win
+        if (n % 4 == 0) {
+            return false;
+        }
+        // Otherwise, you can win
+        return true;
+    }
+}
+
+
+
+
+//C++ Code //
+/* 
+
+#include <iostream>
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        // If the number of stones is a multiple of 4, you cannot win
+        if (n % 4 == 0) {
+            return false;
+        }
+        // Otherwise, you can win
+        return true;
+    }
+};
+
+
+*/
+
+
+
+
+// python code
+
+/*
+  class Solution:
+    def canWinNim(self, n: int) -> bool:
+        # If the number of stones is a multiple of 4, you cannot win
+        if n % 4 == 0:
+            return False
+        # Otherwise, you can win
+        return True
+
+
+ */
