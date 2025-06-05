4bf77df7378d8b7e09cab5d6ee8f2b31ebd7bf2a
arpit jain <128150708+arpitjaingwl7@users.noreply.github.com>
2025-06-05 20:47:28 +0530

    Time: 3 ms (32.79%), Space: 45.1 MB (96.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,17 +3,37 @@ class Solution {
         int n=board.length;
         int m=board[0].length;
         int[][]vis=new int[n][m];
+         Queue<Pair>q=new ArrayDeque<>();
 
 
+       
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
 
                 if((i==0||j==0||i==n-1||j==m-1)&& board[i][j]=='O'&&vis[i][j]==0){
 
-                    dfs(board,vis,i,j,n,m);
+                    vis[i][j]=1;
+                    q.add(new Pair(i,j));
+
 
                 }
             }
+        }
+        while(q.size()!=0){
+            Pair temp=q.remove();
+            int dx[]={0,0,-1,1};
+            int dy[]={1,-1,0,0};
+
+            for(int i=0;i<4;i++){
+                int r=temp.x+dx[i];
+                int c=temp.y+dy[i];
+
+                if(r>=0 && c>=0 && r<n && c<m && vis[r][c]==0 && board[r][c]=='O'){
+                    vis[r][c]=1;
+                    q.add(new Pair(r,c));
+                }
+            }
+
         }
 
         for(int i=0;i<n;i++){
@@ -27,24 +47,13 @@ class Solution {
 
 
     }
-    void dfs(char[][]board,int[][]vis,int i,int j,int n,int m){
-    
-        vis[i][j]=1;
 
-        int []dx={1,-1,0,0};
-        int[]dy={0,0,1,-1};
-
-        for(int l=0;l<4;l++){
-           int r=i+dx[l];
-            int c=j+dy[l];
-
-            if(r>=0&&c>=0&&r<n&&c<m&&board[r][c]=='O'&& vis[r][c]==0){
-                dfs(board,vis,r,c,n,m);
-            }
-        }
-
-
-
-
-    }
+}
+class Pair{
+int x;
+int y;
+Pair(int i,int j){
+    x=i;
+    y=j;
+}
 }
