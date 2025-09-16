4542ed0155b6936a7fac0a838d3f31bdf8c6a13a
arpit jain <128150708+arpitjaingwl7@users.noreply.github.com>
2025-09-16 12:14:05 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,25 @@
 class Solution {
-    int []arr=new int[46];
 
+    int []dp=new int[46];
 
     public int climbStairs(int n) {
-
-      if(arr[n]!=0){
-        return arr[n];
-      }
-        // Base cond
-
+        // base case
         if(n==1){
             return 1;
         }
         if(n==2){
             return 2;
         }
-    
-    arr[n]=climbStairs(n-1)+climbStairs(n-2);
+        if(dp[n]!=0){
+            return dp[n];
+        }
 
-        return  arr[n];
+        // Rec Call
+        int a=climbStairs(n-1);
+        int b=climbStairs(n-2);
+
+        // Some work
+        dp[n]=a+b;
+        return dp[n];
     }
 }
