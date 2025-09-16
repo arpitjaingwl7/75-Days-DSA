cd49f9043ea5317c44bb6d4f343e95c1546e9e48
arpit jain <128150708+arpitjaingwl7@users.noreply.github.com>
2025-09-16 15:59:31 +0530

    Time: 0 ms (100%), Space: 40.5 MB (80.81%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int trailingZeroes(int n) {
+
+
+       int ans=0;
+       int a=5;
+
+       while(a<=n){
+        ans+= (n/a);
+        a=a*5;
+       } 
+       return ans;
+    }
+}
