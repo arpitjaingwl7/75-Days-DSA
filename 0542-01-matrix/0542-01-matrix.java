7255609a699cebde3a40f587294cf8429c1ceaec
arpit jain <128150708+arpitjaingwl7@users.noreply.github.com>
2026-01-04 00:42:52 +0530

    Time: 13 ms (92.37%), Space: 48.9 MB (68.74%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,49 +1,59 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-      
-      int m=mat.length;
-      int n=mat[0].length;
-      int [][]vis=new int [m][n];
-       Queue<Pair>q=new ArrayDeque<>();
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
 
+ int [][]vis=new int[mat.length][mat[0].length];
+       Queue<Pair>q=new ArrayDeque<>();
+
+       for(int i=0;i<mat.length;i++){
+        for(int j=0;j<mat[0].length;j++){
             if(mat[i][j]==0){
                 vis[i][j]=1;
                 q.add(new Pair(i,j,0));
             }
         }
        } 
-       
+
+       int[] dx={-1,1,0,0};
+       int []dy={0,0,-1,1};
+
+
+
        while(q.size()!=0){
-        Pair temp=q.remove();
 
-       int []dx={1,-1,0,0};
-       int[]dy={0,0,-1,1};
+        Pair p=q.remove();
+        int dis= p.dis;
+           
+        for(int x=0;x<4;x++){
+            int row=p.i+dx[x];
+            int col=p.j+dy[x];
 
-       for(int i=0;i<4;i++){
-        int r=temp.x+dx[i];
-        int c=temp.y+dy[i];
+            if(row>=0&&col>=0&&row<mat.length&&col<mat[0].length&&vis[row][col]!=1&& mat[row][col]==1){
 
-        if(r>=0 && c>=0 && r<m && c<n && vis[r][c]==0 ){
-            vis[r][c]=1;
-            mat[r][c]=temp.t+1;
-            q.add(new Pair(r,c,temp.t+1));
-                    
+                 mat[row][col]=dis+1;
+                 vis[row][col]=1;
+
+                 q.add(new Pair(row,col,dis+1));
+
+            }
+
         }
-       }
+
 
        }
-       return mat;
+         return mat;
     }
+  
 }
 class Pair{
-    int x;
-    int y;
-    int t;
-    Pair(int i,int j,int k){
-        x=i;
-        y=j;
-        t=k;
+    
+    int i;
+    int j;
+    int dis;
+
+    Pair(int x,int y,int d){
+      
+      i=x;
+      j=y;
+      dis=d;
     }
 }
